7ada28b8e0573e9085777a046dd2031c50e7b7af
Alejandro Leyva <A.Leyvaa95@gmail.com>
2020-07-17 02:49:21 -0600

    Se agregaron las plantillas donde iran los personajes, el hom y el header

---
+++ src/pages/Character.js
@@ -0,0 +1,13 @@
+const Character =  () => {
+  const view = `
+  <section class="Characters__container">
+    <div class="Characters__inner">
+      <h1>Character</h1>
+    </div>
+  </section>
+
+  `
+   return view;
+};
+
+export default Character;
---
+++ src/templates/Header.js
@@ -0,0 +1,24 @@
+const Header = () => {
+  const view = ` 
+    <div class="Header__main">
+        <div class="Header-logo">
+            <h1>
+            <a href="/">
+            Amiibos.io
+            </a>
+            </h1>
+        </div>
+        <div>
+            <img class="Main__logo" src="" alt="Logo">
+        </div>
+        <div class="Header-nav">
+            <a href="#/about/">
+                About
+            </a>
+        </div>
+    </div>
+    `;
+    return view;
+};
+  
+export default Header;
